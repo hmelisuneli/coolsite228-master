f3d0b0e1532c7a06c9d53005a77b212f73beb9c0
hmelisuneli <112614170+hmelisuneli@users.noreply.github.com>
2023-03-24 22:18:06 +0600

    123

--- women/views.py
+++ women/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .forms import *
 from .models import *
@@ -10,18 +11,34 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Войти", 'url_name': 'login'}
 ]
 
+class HeroHome(ListView):
+    model = Women
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
 
-def index(request):
-    posts = Women.objects.all()
 
-    context = {
-        'posts': posts,
-        'menu': menu,
-        'title': 'Главная страница',
-        'cat_selected': 0,
-    }
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Главная страница'
+        context['cat_selected'] = 0
+        return context
 
-    return render(request, 'women/index.html', context=context)
+    def get_queryset(self):
+        return Women.objects.filter(is_published=True)
+
+
+#def index(request):
+#   posts = Women.objects.all()
+#
+#   context = {
+#       'posts': posts,
+#      'menu': menu,
+#       'title': 'Главная страница',
+ #       'cat_selected': 0,
+ #   }
+#
+#   return render(request, 'women/index.html', context=context)
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
@@ -29,11 +46,11 @@ def about(request):
 
 def addpage(request):
     if request.method == 'POST':
-        form = AddPostForm(request.POST)
+        form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
             #print(form.cleaned_data)
             try:
-                Women.objects.create(**form.cleaned_data)
+                form.save()
                 return redirect('home')
             except:
                 form.add_error(None, 'Ошибка добавления поста')
@@ -64,18 +81,34 @@ def show_post(request, post_slug):
 
     return render(request, 'women/post.html', context=context)
 
-def show_category(request, cat_id):
-    posts = Women.objects.filter(cat_id=cat_id)
+class HeroCategory(ListView):
+    model = Women
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
 
-    if len(posts) == 0:
-        raise Http404()
+    def get_queryset(self):
+        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
 
-    context = {
-        'posts': posts,
-        'menu': menu,
-        'title': 'Отображение по рубрикам',
-        'cat_selected': cat_id,
-    }
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Категория -' + str(context['posts'][0].cat)
+        context['menu'] = menu
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
 
-    return render(request, 'women/index.html', context=context)
+#def show_category(request, cat_id):
+#    posts = Women.objects.filter(cat_id=cat_id)
+#
+ #   if len(posts) == 0:
+  #      raise Http404()
+#
+ #   context = {
+  #      'posts': posts,
+   #     'menu': menu,
+    #    'title': 'Отображение по рубрикам',
+     #   'cat_selected': cat_id,
+    #}#
+
+    #return render(request, 'women/index.html', context=context)
 
