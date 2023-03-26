6106124a44c68555df36b189e77ad95f89f8fefd
hmelisuneli <112614170+hmelisuneli@users.noreply.github.com>
2023-03-26 14:38:37 +0600

    123

--- women/views.py
+++ women/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView
@@ -8,6 +9,7 @@ from .models import *
 from .utils import *
 
 class HeroHome(DataMixin, ListView):
+    paginate_by = 3
     model = Women
     template_name = 'women/index.html'
     context_object_name = 'posts'
@@ -38,7 +40,12 @@ class HeroHome(DataMixin, ListView):
 #   return render(request, 'women/index.html', context=context)
 
 def about(request):
-    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+    contact_list = Women.objects.all()
+    paginator = Paginator(contact_list, 3)
+
+    page_number = request.GET.get('page')
+    page_obj =  paginator.get_page(page_number)
+    return render(request, 'women/about.html', {'page_obj':page_obj, 'menu': menu, 'title': 'О сайте'})
 
 
 class AddPage(LoginRequiredMixin, DataMixin, CreateView):
